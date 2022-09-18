216095ae3b8afe19ce2ecb58aa190023bd773ca8
rafaeelnunes <rafaelnfsq@gmail.com>
2022-09-18 14:05:44 -0300

    feat: add component Alert in the App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import { AlertProvider } from "./contexts/AlertContext";
+import Alert from "./components/Alert";
 import SignUp from "./pages/signUp/signUp";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
+    <AlertProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+      <Alert />
+    </AlertProvider>
   );
 }
 
