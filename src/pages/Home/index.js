f744bce3b4aef26d2b4fa30e2844beeb50144f92
rafaeelnunes <rafaelnfsq@gmail.com>
2022-09-18 18:53:51 -0300

    feat: add initial layout to home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+
+import { useEffect } from "react";
 function Home() {
-  return <h1>Hello</h1>;
+  let navigate = useNavigate();
+  const { logout, displayName } = useAuth();
+
+  useEffect(() => {
+    if (!displayName) return navigate("/sign-in");
+  }, []);
+
+  function handleClose() {
+    logout();
+    navigate("/sign-in");
+  }
+
+  return (
+    <>
+      <h1>Hello {displayName}</h1>
+      <button onClick={handleClose}>Logout</button>
+    </>
+  );
 }
 
 export default Home;
