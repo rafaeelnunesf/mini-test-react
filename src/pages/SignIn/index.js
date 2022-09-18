d66d39cf8c64424e1c406c688993004ce449e67d
rafaeelnunes <rafaelnfsq@gmail.com>
2022-09-18 15:28:40 -0300

    feta: create SigIn form

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,50 @@
-function SignIn() {
-  return <h1>Hello</h1>;
-}
+import { TextField, Typography, Link, Button } from "@mui/material";
+import { Box } from "@mui/system";
+import { useState } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import Form from "../../components/Form";
+import PasswordInput from "../../components/PasswordInput";
+import { styles } from "../../styles/authStyles";
 
+function SignIn() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleInputChange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+  return (
+    <Form>
+      <Box sx={styles.container}>
+        <Typography sx={styles.title} variant="h4" component="h1">
+          Login
+        </Typography>
+        <TextField
+          name="email"
+          sx={styles.input}
+          label="Email"
+          type="email"
+          variant="outlined"
+          onChange={handleInputChange}
+          value={formData.email}
+        />
+        <PasswordInput
+          name="password"
+          sx={styles.input}
+          label="Password"
+          onChange={handleInputChange}
+          value={formData.password}
+        />
+        <Button variant="contained" type="submit" sx={{ marginBottom: "16px" }}>
+          Log In
+        </Button>
+        <Link component={RouterLink} to="/sign-up">
+          <Typography>I don't have an account!</Typography>
+        </Link>
+      </Box>
+    </Form>
+  );
+}
 export default SignIn;
