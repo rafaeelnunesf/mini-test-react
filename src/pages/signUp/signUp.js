3fbefbe8c75fd90b61b2e74eb001968e9b344d7e
rafaeelnunes <rafaelnfsq@gmail.com>
2022-09-18 13:45:25 -0300

    feat: add submit function on signUp page

--- src/pages/signUp/signUp.js
+++ src/pages/signUp/signUp.js
@@ -1,12 +1,14 @@
 import { TextField, Typography, Link, Button } from "@mui/material";
 import { Box } from "@mui/system";
 import { useState } from "react";
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
 import Form from "../../components/Form";
 import PasswordInput from "../../components/PasswordInput";
+import api from "../../services/api";
 import { styles } from "./styles";
 
 function SignUp() {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -15,8 +17,50 @@ function SignUp() {
   function handleInputChange(e) {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (
+      !formData?.email ||
+      !formData?.password ||
+      !formData?.passwordConfirmation
+    ) {
+      console.log({ type: "error", text: "All fields are mandatory!" });
+      return;
+    }
+
+    const { email, password, passwordConfirmation } = formData;
+
+    if (password !== passwordConfirmation) {
+      console.log({ type: "error", text: "Passwords must be the same!" });
+      return;
+    }
+
+    try {
+      const user = await api.register({ email, password });
+      console.log(user);
+      console.log({
+        type: "success",
+        text: "Registration successfully Complete!",
+      });
+      navigate("/home");
+    } catch (error) {
+      if (error.response) {
+        console.log({
+          type: "error",
+          text: error.response.data,
+        });
+        return;
+      }
+      console.log(error);
+      console.log({
+        type: "error",
+        text: "Error, try again in a few seconds!",
+      });
+    }
+  }
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Box sx={styles.container}>
         <Typography sx={styles.title} variant="h4" component="h1">
           Create account
